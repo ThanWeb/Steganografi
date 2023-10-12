3b361fefc9d1171776e23eed98d144b440ef67e8
Hans <classsickpeople@gmail.com>
2023-10-12 13:13:43 +0800

    Read pixel in image and write rgb on some point of cover image

--- Tugas/Tugas-2/image.py
+++ Tugas/Tugas-2/image.py
@@ -1,10 +1,21 @@
 from PIL import Image
 
-img = Image.open(r"Lenna.jpeg")
+img = Image.open('example.jpeg')
+img_d = img.getdata()
 width, height = img.size
 
-for x in range(width):
-  for y in range(1):
-    coordinate = x, y
-    print (img.getpixel(coordinate))  
+cover = Image.open('cover.bmp')
+cover_d = cover.getdata()
+
+new_image = []
+
+for index, x in enumerate(img_d):
+  if (index % 2) == 0:
+    new_image.append(img_d[index])
+  else:
+    new_image.append(cover_d[index])
+
+# print(new_image)
+cover.putdata(new_image)
+cover.save('output.bmp')
   
