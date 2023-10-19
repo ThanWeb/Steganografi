0403f55fc48a11d39def4796b8a52591e8fa2423
Hans <classsickpeople@gmail.com>
2023-10-20 00:29:25 +0800

    Delete console

--- Tugas/2008561030-Tugas_1/image.py
+++ Tugas/2008561030-Tugas_1/image.py
@@ -1,24 +1,5 @@
 from PIL import Image
 
-# img = Image.open('example.jpeg')
-# img_d = img.getdata()
-# width, height = img.size
-
-# cover = Image.open('cover.bmp')
-# cover_d = cover.getdata()
-
-# new_image = []
-
-# for index, x in enumerate(img_d):
-#   if (index % 2) == 0:
-#     new_image.append(img_d[index])
-#   else:
-#     new_image.append(cover_d[index])
-
-# # print(new_image)
-# cover.putdata(new_image)
-# cover.save('output.bmp')
-
 def encode():
   inputImage = input("Enter image name(with extension): ")
   image = Image.open(inputImage, 'r')
